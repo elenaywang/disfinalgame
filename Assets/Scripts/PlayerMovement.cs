95f5b3ad8a88a29ec9abae1865f4f026d58cfd22
Elena Wang <ewang88@smith.edu>
2025-05-05 19:27:55 +0200

    restrict player movement to only within the screen

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
+// CONTAINS CODE FROM: Wild Cockatiel Games https://youtu.be/qnr42UXQ0kc?si=pJbaleu7VR57bzBD
+
 public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 3; 
 
+    // for determining which set of keys are used to control  movement
     public KeyCode inputLeft;
 
     Rigidbody2D rb2d;
     private PlayerEnergy pe;
+
+    // for restricting player movement within the screen
+    private Vector2 screenBounds;
+    private float playerHalfWidth;
+    private float playerHalfHeight;
 
     [Header("Animation")]
     public Sprite spriteSide;
@@ -23,15 +31,32 @@ public class PlayerMovement : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
         pe = GetComponent<PlayerEnergy>();
         _playerSR = GetComponent<SpriteRenderer>();
+
+        // for restricting player movement within the screen
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        playerHalfWidth = GetComponent<SpriteRenderer>().bounds.extents.x;
+        playerHalfHeight = GetComponent<SpriteRenderer>().bounds.extents.y;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (!pe.isAwake) return;           // player doesn't move if sleeping
+        // player doesn't move if sleeping
+        if (!pe.isAwake) {
+            rb2d.linearVelocity = new Vector2(0, 0);
+            return;
+        }
 
         MovePlayer();
+
+        // for restricting player movement within the screen
+        float clampedX = Mathf.Clamp(transform.position.x, -screenBounds.x + playerHalfWidth, screenBounds.x - playerHalfWidth);
+        float clampedY = Mathf.Clamp(transform.position.y, -screenBounds.y + playerHalfHeight, screenBounds.y - playerHalfHeight);
+        Vector2 pos = transform.position;       // get player's current position
+        pos.x = clampedX;                       // reassign x value to clamped x position
+        pos.y = clampedY;                       // reassign y value to clamped y position
+        transform.position = pos;               // reassign the clamped value back to the player
     }
 
 
